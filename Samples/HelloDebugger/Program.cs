18935c3177a0174ba9f17c74c8ee697e63844d3d
Tom Dowdell <tom@lesarde.com>
2018-05-03 18:13:07 -0700

    Added Samples.sln

---
+++ Samples/HelloDebugger/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Demonstrates how to debug a frogui app. It:
+ *		- is a C# .NET Standard dll project -- the only option at the moment for frogui.
+ *		- has an <OutputType> property added to the project file so the "Main" method entry point can be found; dll's do not have entry points by default.
+ *		- has the Lesarde.Frogui.Wasm NuGet package added.
+ *		- may be run as-is within a browser.
+ *		- is meant to be debugged using the HelloDebuggerWin project.
+ */
+
+using Lesarde;
+using System.Threading.Tasks;
+
+namespace HelloDebugger
+{
+	// Make public so that HelloDebuggerWin can access
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			// Call async method
+			MainAsync(args);
+		}
+
+		// This is the body of the Main method, made public and async so it can be called by a Windows Console app for debugging purposes
+		public static async void MainAsync(string[] args)
+		{
+			Console.WriteLine("Hello, Debugger!");
+
+			// Await nothing so compiler don't warn
+			await Task.CompletedTask;
+		}
+	}
+}
---
+++ Samples/HelloDebuggerWin/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Demonstrates how to debug a frogui app. It:
+ *		- is a C# .NET Core app project.
+ *		- references HelloDebugger project.
+ *		- may be run in Visual Studio's debugger.
+ */
+
+namespace HelloDebuggerWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Call into HelloDebugger
+			HelloDebugger.Program.Go(args);
+        }
+    }
+}
